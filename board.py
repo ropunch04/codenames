f7fd448555c729aae6a883d219c485dcb1e9dd16
ropunch04 <837329@lcps.org>
2021-02-14 18:33:01 -0500

    screen color change

--- board.py
+++ board.py
@@ -5,10 +5,4 @@ class Board():
     
     def setBoard(self):
         self.board = []
-        for row in range(self.size[0]):
-            row = []
-            for _ in range(self.size[1]):
-                word = ["Test", "hello", "hola", "afdf"]
-                row.append(x for x in word)
-            self.board.append(row)
-        print(self.board)
+
--- game.py
+++ game.py
@@ -1,15 +1,35 @@
 import pygame
 
 class Game():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 800))
+    screen.fill([0, 0, 0])
+
     def __init__(self, board):
         self.board = board
+
+    def updateScreen(self, color):
+        self.screen.fill(color)
+        pygame.display.flip()
+
     def run(self):
-        pygame.init()
-        pygame.display.set_mode((800, 800))
         running = True
+        r, g, b = 0, 0, 0
         while running:
             for event in pygame.event.get():
                 if (event.type == pygame.QUIT):
                     running = False
-            pygame.display.flip()
+            for r in range(256):
+                COLOR  = [r, g, b]
+                self.updateScreen(COLOR)
+            for g in range(256):
+                COLOR = [r, g, b]
+                self.updateScreen(COLOR)
+            for b in range(256):
+                COLOR  = [r, g, b] 
+                self.updateScreen(COLOR)
+            print("RED: ", r)
+            print("GREEN: ", g)
+            print("BLUE: ", b)
         pygame.quit()
+
