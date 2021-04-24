6b02aa02c7adb6f55388db96c3103f1f86a97587
ropunch04 <837329@lcps.org>
2021-04-23 23:51:40 -0400

    Added Spymaster Function
    future: try to implement double window??, add reset button in top left, change words button, try and implement seed???

--- game.py
+++ game.py
@@ -7,7 +7,11 @@ class Game:
     pg.init()
     pg.display.set_caption('Codenames')
     screen = pg.display.set_mode((0,0), pg.FULLSCREEN)
+    # screen = pg.display.set_mode((1390,800))
     screen.fill([255, 255, 255])
+
+    font1 = pg.font.SysFont('times', 30)
+    font2 = pg.font.SysFont('times', 60)
 
     f = open("words.txt")
 
@@ -23,6 +27,10 @@ class Game:
         self.assign_colors(random_c, self.box_arr)
         random_t = self.randomize_array()
         self.assign_text(random_t, self.box_arr)
+        self.spymaster = pg.draw.rect(self.screen, [200, 200, 200], (1175, 25, 210, 40))
+        text_obj = self.font1.render('Spymaster', True, (0,0,0))
+        self.screen.blit(text_obj, (1220, 30))
+        self.is_master = False
 
 
     def randomize_array(self):
@@ -54,6 +62,39 @@ class Game:
         for elem in rand_arr:
             arr[elem].set_text(random.choice(words))
 
+    def confirmation(self):
+        leave = False
+        self.screen.fill([255, 255, 255])
+        conf_text = self.font2.render('Are you sure you want to reveal all colors?', True, (0, 0, 0))
+        self.screen.blit(conf_text, (185, 175))
+
+        red = pg.draw.rect(self.screen, [255, 0, 0], (500, 300, 150, 150))
+        red_text = self.font2.render('No', True, (0, 0, 0))
+        self.screen.blit(red_text, (540, 340))
+
+        green = pg.draw.rect(self.screen, [0, 255, 0], (800, 300, 150, 150))
+        green_text = self.font2.render('Yes', True, (0, 0, 0))
+        self.screen.blit(green_text, (825, 340))
+
+        while (not leave):
+            for event in pg.event.get():
+                x, y = event.pos
+                if (event.type == pg.MOUSEBUTTONDOWN):
+                    if red.collidepoint(x, y):
+                        returnVal = False
+                        leave = True
+                    elif green.collidepoint(x, y):
+                        returnVal = True
+                        leave = True
+            pg.display.flip()
+        self.screen.fill([255, 255, 255])
+        return returnVal
+
+    def show_spymaster(self):
+        for box in self.box_arr:
+            box.reveal_color()
+        self.is_master = True
+
     def run(self):
         running = True
         while running:
@@ -65,9 +106,17 @@ class Game:
                     running = False
                 elif (event.type == pg.MOUSEBUTTONDOWN):
                     x, y = event.pos
-                    for z in self.box_arr:
-                        if z.get_rect().collidepoint(x, y):
-                            z.reveal_color()
+                    for box in self.box_arr:
+                        if box.get_rect().collidepoint(x, y):
+                            box.reveal_color()
+                    if (self.spymaster.collidepoint(x, y) and self.is_master == False):
+                        reveal = self.confirmation()
+                        if (reveal):
+                            self.show_spymaster()
+                        else:
+                            self.spymaster = pg.draw.rect(self.screen, [200, 200, 200], (1175, 25, 210, 40))
+                            text_obj = self.font1.render('Spymaster', True, (0,0,0))
+                            self.screen.blit(text_obj, (1220, 30))
                 for x in self.box_arr:
                     if x.get_rect().collidepoint(pg.mouse.get_pos()):
                         x.large_text()
